1de9fc93f4430bfb05947df50adb08ad3a8c9e7c
hrenxiang <2295701930@qq.com>
2023-03-17 17:57:05 +0800

    first commit

---
+++ src/main/java/cn/huangrx/text/HuangrxTextApplication.java
@@ -0,0 +1,20 @@
+package cn.huangrx.text;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 启动类
+ *
+ * @author   huangrx
+ * @since   2023-03-17 17:55
+ */
+@SpringBootApplication
+public class HuangrxTextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuangrxTextApplication.class, args);
+    }
+
+}
